e6f1f34f082438c6f0227bca398440a1d86c19de
collegedude9300 <collegenm2012@gmail.com>
2024-12-26 20:55:10 -0800

    font

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,13 +1,8 @@
-import Link from "next/link";
 export function Intro() {
-  
-
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12" aria-label="Main content" lang="en">
-      <h3 className="text-2xl md:text-3xl font-bold tracking-tighter leading-tight md:pr-8" title="Spectrum Station Life Skills Blog">
+      <h3 className="text-xl md:text-2xl font-bold tracking-tighter leading-tight md:pr-8" title="Spectrum Station Life Skills Blog">
          Spectrum Station Life Skills  Blog LIFE 101 and 102 classes.</h3>
-         <h4 className="text-2xl md:text-3xl font-bold tracking-tighter leading-tight md:pr-8">
-          Foundation of Life Skills</h4>
     </section>
   );
 }
