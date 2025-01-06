f8490899964545447d487bb2d7ae0fd95f6dc4de
collegedude9300 <collegenm2012@gmail.com>
2025-01-05 18:41:46 -0800

    update update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
 import Link from "next/link";
-
 export default function Index() {
   const allPosts = getAllPosts();
 
   const heroPost = allPosts[0];
 
   const morePosts = allPosts.slice(1);
-
   return (
     <main>
       <Container>
@@ -25,9 +23,10 @@ export default function Index() {
           excerpt={heroPost.excerpt}
         />
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        <Link href="https://zenstationnext.vercel.app/" className="block md:inline-block text-center md:text-left text-sm md:text-base font-medium tracking-wider uppercase transition duration-300 hover:text-gray-900" title="Visit Spectrum Station Life">
-     Check out Life 103 for our cooking class!
-   </Link>
+
+        <Link href={`/posts/${heroPost.slug}`} className="btn btn-primary">//+
+          Read More//+
+        </Link>//+
       </Container>
     </main>
   );
