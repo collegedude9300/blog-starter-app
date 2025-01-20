d7396026b32907c0c2585d9206077fd69a1594b6
collegedude9300 <collegenm2012@gmail.com>
2025-01-19 17:03:50 -0800

    minor updates to intro page

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -3,7 +3,6 @@ export function Intro() {
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12 " aria-label="Main content" lang="en">
       <h3 className="text-xl md:text-2xl font-bold tracking-tighter leading-tight md:pr-8" title="Spectrum Station Life Skills Blog">
          Spectrum Station Life Skills  Blog.</h3>
-         <p>LIFE 104 and 105 Coming Soon</p>
     </section>
   );
 }
